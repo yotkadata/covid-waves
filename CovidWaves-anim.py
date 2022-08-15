e431e90002194406b72a42dfc3499145053b5a4a
yan <yan@riseup.net>
2022-08-14 21:29:36 -0400

    Initial Commit

---
+++ CovidWaves-anim.py
@@ -0,0 +1,469 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import datetime as dt
+import json
+import imageio
+import pathlib
+import glob
+import time
+
+#
+# Define some variables for the script
+#
+
+set_dates = False  # Use date_start and date_end to limit the dataset?
+date_start = '2022-01-01'  # Start date if 'set_dates' is True
+date_end = '2022-05-01'  # End date if 'set_dates' is True
+
+mode = 'png'  # html, png or stitch (manual_path)
+resolution = '10M'  # Resolution for the map: 01M, 03M, 10M, 60M
+metric = 'moving14d_pop'  # Metric to use: moving7d_pop, moving14d_pop, cumulated_pop, cases_pop_weekly, moving4w_pop
+metric_desc = {  # Descriptions for the different metrics
+    'moving7d_pop': 'Moving 7 day average of detected cases per million by NUTS region',
+    'moving14d_pop': 'Moving 14 day average of detected cases per million by NUTS region',
+    'cumulated_pop': 'Cumulated detected cases per million by NUTS region',
+    'cases_pop_weekly': 'Weekly detected cases per million by NUTS region',
+    'moving4w_pop': 'Moving 4 week average of detected weekly cases per million by NUTS region',
+}
+
+animation = True  # Create animation? True or False (just for mode 'png')
+manual_path = '/home/jan/DataViz/Projekte/CovidWaves/export/png/20220814-143236/'  # Path for manual
+animation_fps = 14  # Frames per second
+animation_loops = 1  # Number of loops (0=loop indefinitely)
+
+height = 800  # Height of the images/animation
+width = 1000  # Width of the images/animation
+colorscale = 'dataset'  # Set colorscale based on 'sample' or whole 'dataset'
+coloraxis = False  # Show color axis? True or False
+
+# white-bg, open-street-map, carto-positron, carto-darkmatter, stamen-terrain, stamen-toner, stamen-watercolor
+basemap = 'white-bg'
+
+##
+
+# Set variables to calculate script running time
+start = time.time()  # Start time to calculate script running time
+dates_processed = 0  # Create empty list for calculation
+
+
+#
+# Define functions
+#
+
+# Calculate quintiles for the colorscale
+def calc_quintiles(df, column):
+    # Steps to be used as break points
+    steps = [0, 0.2, 0.4, 0.6, 0.8, 0.9, 0.95, 0.99, 1]
+    breaks = {}
+
+    for step in range(len(steps)):
+        breaks[steps[step]] = (df[column].quantile(steps[step]) / df[column].max()).round(3)
+
+    return breaks
+
+
+# Stitch images to get an animation
+def stitch_animation(image_files, path, params='', fps=animation_fps):
+    print("Starting to stitch images together for an animation.")
+
+    images = []
+    image_count = 0
+
+    # Loop through image files and add them to 'images'
+    for file_name in image_files:
+        images.append(imageio.v2.imread(file_name))
+        image_count += 1
+        print("Done. Added", image_count, "images.")
+
+    print("Create animation.")
+
+    # Join parameters to be added to file name
+    try:
+        iter(params)
+        file_params = '-' + '-'.join(params)
+    except TypeError:
+        print("{} is not iterable".format(params))
+        file_params = ''
+
+    # Create path and file name for animation
+    gif_path = str(path) + '/animation' + file_params + '-fps' + str(fps) + '.gif'
+
+    # Create animation
+    imageio.mimsave(gif_path, images, fps=fps, loop=animation_loops)
+
+    print("Animation saved to", gif_path)
+
+
+##
+
+#
+# Define custom template for Plotly output
+#
+
+custom_template = {
+    'layout': go.Layout(
+        font={
+            'family': 'Lato',
+            'size': 12,
+            'color': '#1f1f1f',
+        },
+        title={
+            'font': {
+                'family': 'Lato',
+                'size': 24,
+                'color': '#1f1f1f',
+            },
+        },
+    )
+}
+
+##
+
+#
+# Import GeoJson files
+#
+
+print("\nImporting geo data.")
+
+# Get geo data for NUTS regions (level 3)
+file_name = 'data/NUTS_RG_' + resolution + '_2016_4326.geojson'
+geo_nuts_level3 = json.load(open(file_name, 'r'))
+
+# Get geo data for countries
+file_name = 'data/CNTR_RG_' + resolution + '_2016_4326.geojson'
+geo_countries = json.load(open(file_name, 'r'))
+
+print("Done.")
+
+##
+
+#
+# Import Covid19 data from CSV
+#
+
+print("\nStarting import of CSV file.")
+
+# Define string to be added to fór weekly metrics
+append = '-weekly' if metric in ['cases_pop_weekly', 'moving4w_pop'] else ''
+
+# Define file name to be imported
+file = 'data/covid-waves-data-clean' + append + '.csv'
+
+# Import CSV
+df_raw = pd.read_csv(file,
+                     parse_dates=['date'],
+                     usecols=['country', 'nuts_id', 'nuts_name', 'date', metric],
+                     header=0,
+                     )
+
+print("File imported:", file)
+
+# Create a copy of the dataframe to work with
+df = df_raw.copy()
+
+##
+
+#
+# If set, reduce data set to requested time frame
+#
+
+if set_dates:
+    df = df[(df['date'] >= date_start) & (df['date'] <= date_end)]
+
+##
+
+#
+# Export maps as PNGs if selected mode is PNG
+#
+
+if mode == 'png':
+
+    # Create folder
+    path = 'export/png/' + str(dt.datetime.now().strftime('%Y%m%d-%H%M%S'))
+    export_path = pathlib.Path(path)
+    export_path.mkdir(parents=True, exist_ok=True)
+
+    image_files = []
+
+    # Calculate quintiles for the colorscale using whole or reduced dataframe
+    df_breaks = df if colorscale == 'sample' else df_raw
+    breaks = calc_quintiles(df_breaks, metric)
+
+    print("\nStart plotting.")
+
+    # Get all unique dates and sort them
+    dates = df['date'].sort_values().unique()
+
+    # Get min and max dates of the whole dataset
+    first_date = df_raw['date'].min()
+    last_date = df_raw['date'].max()
+
+    for date in dates:
+
+        # Set variable to track performance
+        duration_start = time.time()
+
+        # Convert date to Pandas datetime
+        date = pd.to_datetime(date)
+
+        # Check if this is the last iteration
+        last_run = True if (len(dates) > 1 and date == dates.max()) else False
+
+        # Create a new dataframe containing just the rows for the current date
+        df_plot = df[df['date'] == date]
+
+        # Calculate position of the date
+        total_seconds = (last_date - first_date).total_seconds()
+        now_seconds = (date - first_date).total_seconds()
+        date_position = 1 - (now_seconds / total_seconds)
+
+        # Start plotting
+        fig = go.Figure(go.Choroplethmapbox(
+            geojson=geo_nuts_level3,
+            locations=df_plot['nuts_id'],
+            z=df_plot[metric],
+            zmin=0,
+            zmax=df_breaks[metric].max(),
+            colorscale=[
+                [0, '#ccc'],
+                [breaks[0.2], '#FFF304'],  # Cadmium Yellow
+                [breaks[0.4], '#FFAC00'],  # Chrome Yellow
+                [breaks[0.6], '#FF4654'],  # Sunburnt Cyclops
+                [breaks[0.8], '#E71827'],  # Pigment Red
+                [breaks[0.9], '#C064E0'],
+                [breaks[0.95], '#9C51B6'],  # Purple Plum
+                [breaks[0.99], '#733381'],  # Maximum Purple
+                [1, '#000000']
+            ],
+        ))
+
+        fig.update_layout(
+            autosize=False,
+            height=height,
+            width=width,
+            mapbox={
+                'center': {'lat': 57.245936, 'lon': 9.274491},  # Set center coordinates of the map
+                'style': basemap,
+                'zoom': 3,
+                'layers': [
+                    {
+                        'name': 'country_borders',  # Add country borders as thin lines
+                        'source': geo_countries,
+                        'type': 'line',
+                        'color': '#ccc',
+                        'opacity': 0.3,
+                        'line': {'width': 1}
+                    },
+                ],
+            },
+            margin={'r': 3, 't': 3, 'l': 3, 'b': 3},
+            template=custom_template,
+            title_text='<b>COVID19 waves in Europe</b><br />'
+                       '<sup>' + metric_desc[metric] + '</sup>',
+            title_x=0.01,
+            title_y=0.96,
+            coloraxis_colorbar=dict(title=''),
+            annotations=[
+                dict(
+                    xref='paper',
+                    yref='paper',
+                    x=0.01,
+                    y=0,
+                    showarrow=False,
+                    text='<b>Data:</b> COVID19-European-Regional-Tracker, <b>Graph:</b> Jan Kühn',
+                ),
+                dict(
+                    xref='paper',
+                    yref='paper',
+                    x=0.99,
+                    y=date_position,
+                    showarrow=False,
+                    text='<b>' + str(date.strftime('%d.%m.%Y')) + '</b>',
+                    font={
+                        'size': 24,
+                    },
+                ),
+            ]
+        )
+
+        # Add attribution to the last frame
+        if last_run:
+            fig.add_annotation(
+                dict(
+                    font=dict(
+                        size=24,
+                    ),
+                    x=0.01,
+                    y=0.5,
+                    showarrow=False,
+                    text='<b>By Jan Kühn</b><br /><sup>https://yotka.org | @derjaku</sup>',
+                    xanchor='left',
+                    xref='paper',
+                    yref='paper',
+                    align='left',
+                )
+            )
+
+        fig.update_traces(
+            marker_line_width=0,  # Width of the NUTS borders
+            showscale=coloraxis,
+        )
+
+        # Define file path and name
+        file = str(export_path) + '/' + \
+               date.strftime('%Y-%m-%d') + '-' + \
+               resolution + '-' + \
+               metric + \
+               '.png'
+
+        # Write map to PNG file
+        fig.write_image(file, width=width, height=height)
+
+        # Append image to variable for animation
+        image_files.append(file)
+
+        # Count dates processed and duration
+        dates_processed += 1
+        duration_end = time.time()
+
+        print("Output saved to", file,
+              "(duration:", str(round(duration_end - duration_start, 1)), "seconds)",
+              dates_processed, "of", len(dates), "(", round(dates_processed / len(dates) * 100, 2), "% )")
+
+    print("All images saved.")
+
+##
+
+#
+# Create HTML animation if selected mode is HTML
+#
+
+if mode == 'html':
+    print("\nConvert date to string for slider")
+
+    # Convert date to string for the slider
+    df['date_str'] = df['date'].apply(lambda x: str(x)[0:10])
+
+    # Calculate quintiles for the colorscale using whole or reduced dataframe
+    df_breaks = df if colorscale == 'sample' else df_raw
+    breaks = calc_quintiles(df_breaks, metric)
+
+    print("\nStart plotting.")
+
+    # Define variable for script statistics
+    dates_processed = len(df['date'].unique())
+
+    # Get min and max dates of the whole dataset
+    first_date = df_raw['date'].min()
+    last_date = df_raw['date'].max()
+
+    # Start plotting
+    fig = px.choropleth_mapbox(
+        df,
+        locations='nuts_id',
+        geojson=geo_nuts_level3,
+        color=metric,
+        range_color=[0, df_breaks[metric].max()],
+        color_continuous_scale=[
+            [0, '#ccc'],
+            [breaks[0.2], '#FFF304'],  # Cadmium Yellow
+            [breaks[0.4], '#FFAC00'],  # Chrome Yellow
+            [breaks[0.6], '#FF4654'],  # Sunburnt Cyclops
+            [breaks[0.8], '#E71827'],  # Pigment Red
+            [breaks[0.9], '#C064E0'],
+            [breaks[0.95], '#9C51B6'],  # Purple Plum
+            [breaks[0.99], '#733381'],  # Maximum Purple
+            [1, '#000000']
+        ],
+        mapbox_style=basemap,
+        center={'lat': 57.245936, 'lon': 9.274491},
+        zoom=3,
+        template=custom_template,
+        animation_frame='date_str',
+        animation_group='nuts_id',
+        width=width,
+        height=height,
+    )
+
+    fig.update_layout(
+        title_text='<b>COVID19 waves in Europe</b><br />'
+                   '<sup>' + metric_desc[metric] + '</sup>',
+        title_x=0.01,
+        title_y=0.96,
+        margin={'r': 0, 't': 0, 'l': 0, 'b': 0},
+        coloraxis_showscale=False,
+        coloraxis_colorbar=dict(title=''),
+        annotations=[
+            dict(
+                xref='paper',
+                yref='paper',
+                x=0.01,
+                y=0,
+                showarrow=False,
+                text='<b>Data:</b> COVID19-European-Regional-Tracker, <b>Graph:</b> Jan Kühn',
+            ),
+        ]
+    )
+
+    fig.update_traces(
+        marker_line_width=0,
+    )
+
+    # Define path and file name for export
+    file = 'export/html/' + dt.datetime.now().strftime('%Y%m%d-%H%M%S') + '.html'
+
+    # Save output as HTML
+    fig.write_html(file)
+
+    print("Output saved to", file)
+
+##
+
+#
+# Create GIF file with animation
+#
+
+if animation and mode == 'png':
+    stitch_animation(image_files, export_path, params=[resolution, metric])
+
+##
+
+#
+# If selected, create animation from files in manually defined directory
+#
+
+if mode == 'stitch':
+    # Create folder
+    path = 'export/png/' + str(dt.datetime.now().strftime('%Y%m%d-%H%M%S'))
+    export_path = pathlib.Path(path)
+    export_path.mkdir(parents=True, exist_ok=True)
+
+    # Define path to look for PNG files
+    search_path = manual_path + '/*.png'
+    image_files = glob.glob(search_path)
+
+    # Sort files
+    image_files.sort()
+
+    # Create animation
+    stitch_animation(image_files, path)
+
+##
+
+#
+# Display statistics of script running time
+#
+
+# Grab currrent Ttime after running the script
+end = time.time()
+
+# Subtract start time from end time
+total_time = end - start
+
+print("Script running time: " + str(round(total_time, 2)) + " seconds (" + str(
+    round(total_time / 60, 2)) + " minutes)")
+
+if dates_processed:
+    print(dates_processed, "days have been processed. That's", round(total_time / dates_processed, 2),
+          "seconds per day.")
---
+++ CovidWaves-update-data.py
@@ -0,0 +1,300 @@
+#
+# Import necessary libraries
+#
+
+import pandas as pd
+
+
+##
+
+#
+# Define some variables for the script
+#
+
+limit_dates = False         # Limit the dates to be included? True/False
+date_start = '2020-11-01'   # Start date in case of True
+date_end = '2020-11-30'     # End date in case of True
+
+refresh_source = False      # Download data to refresh? True/False
+
+##
+
+#
+# Get Covid19 data
+#
+
+print("Get Covid19 data.")
+
+# COVID19-European-Regional-Tracker
+# https://github.com/asjadnaqvi/COVID19-European-Regional-Tracker
+
+if refresh_source:
+
+    import requests
+
+    remote_url = 'https://raw.githubusercontent.com/asjadnaqvi/COVID19-European-Regional-Tracker/master/04_master' \
+                 '/csv_nuts/EUROPE_COVID19_master.csv '
+    local_file = 'data/european-regional-tracker.csv'
+
+    # Make http request for remote file data
+    data = requests.get(remote_url)
+
+    # Save file data to local copy
+    with open(local_file, 'wb') as file:
+        file.write(data.content)
+
+
+# Import CSV with Covid19 data
+covid_raw = pd.read_csv('data/european-regional-tracker.csv',
+                        sep=';',
+                        decimal='.',
+                        parse_dates=['date'],
+                        usecols=['country', 'nuts_id', 'nuts_name', 'date', 'population', 'cases_daily'],
+                        header=0,
+                        )
+
+# Set column names
+covid_raw.columns = ['country', 'nuts_id', 'nuts_name', 'date', 'population', 'cases']
+
+print("Import done.")
+
+##
+
+#
+# If selected above, reduce the dataset to selected time frame
+#
+
+if limit_dates:
+    covid_raw = covid_raw[(covid_raw['date'] >= date_start) & (covid_raw['date'] <= date_end)]
+
+##
+
+#
+# Do some cleaning
+#
+
+print("\nDo some cleaning.")
+
+# Make a copy of the dataframe
+covid_clean = covid_raw.copy()
+
+##
+
+# Remove rows with negative values
+print("\nRemove rows with negative values.")
+
+length_before = len(covid_clean)
+covid_clean = covid_clean[covid_clean['cases'] >= 0]
+length_after = len(covid_clean)
+
+print("\nRemoved", length_before - length_after, "rows with values below 0.\n", length_after, "rows left.")
+
+##
+
+# Correct some dates from the years 2121 and 2222
+# https://stackoverflow.com/a/50674062/381821
+
+print("\nCorrect some dates.")
+
+# Define years to be corrected
+error_years = {2121: 2021, 2222: 2022}
+
+# Loop through years to be corrected
+for y in error_years:
+    covid_clean['date'] = (covid_clean['date']
+                           .apply(lambda x: x.replace(year=x.year - (y - error_years[y])) if x.year == y else x))
+
+print("Done.")
+
+
+# Manually remove some detected outliers
+
+print("\nRemove some outliers.")
+
+# Define outliers to be removed
+outliers = dict(ITH10=['2020-10-08', '2021-03-22'], ITF21=['2021-05-22'], ITF22=['2021-05-23'],
+                RO126=['2022-02-21', '2022-02-22', '2022-02-23'], ITF63=['2022-06-17', '2022-06-18'],
+                ITF61=['2022-06-17', '2022-06-18'], EL53=['2020-05-03'], CH031=['2022-06-20'])
+
+# Loop through outliers to remove them
+for nuts_id in outliers:
+    covid_clean = covid_clean.drop(
+        covid_clean[(covid_clean['nuts_id'] == nuts_id) & (covid_clean['date'].isin(outliers[nuts_id]))].index)
+
+print("Done.")
+
+# Sort dataframe
+covid_clean = covid_clean.sort_values(['nuts_id', 'date'])
+
+print("\nCleaning done.")
+
+##
+
+#
+# Do some calculations
+#
+
+print("\nDo some calculations.")
+
+# Make a copy of the dataframe
+covid_calc = covid_clean.copy()
+
+# Add missing dates for each nuts_id group
+# From https://stackoverflow.com/a/62690665
+
+print("\nAdd missing dates for each nuts_id group.")
+
+# Get min and max values for dates in the whole dataset
+date_min = covid_calc['date'].min()
+date_max = covid_calc['date'].max()
+
+# Fill in missing dates for each group
+covid_calc = (covid_calc.set_index('date')
+              .groupby('nuts_id')
+              .apply(lambda x: x.reindex(pd.date_range(date_min, date_max, freq='D', name='date')))
+              .drop('nuts_id', axis=1)
+              )
+# Reset index
+covid_calc = covid_calc.reset_index()
+
+print("Done.")
+
+
+# Fill missing values in 'static' columns (first forwards, than backwards)
+
+print("\nFill missing values in 'static' columns.")
+
+# Columns te be filled
+fill = ['country', 'nuts_id', 'nuts_name', 'population']
+
+# Loop through the columns and fill them
+for fill_col in fill:
+    covid_calc[fill_col] = covid_calc.groupby('nuts_id')[fill_col].ffill().bfill()
+
+print("Done.")
+
+
+# Interpolate missing values in 'dynamic' columns
+# From https://stackoverflow.com/a/58844499
+
+print("\nInterpolate missing values in 'dynamic' columns.")
+
+# Columns to be interpolated
+interpolate = ['cases']
+
+# Loop through columns and interpolate missing values
+for fill_col in interpolate:
+    covid_calc[fill_col] = covid_calc.groupby('nuts_id').apply(lambda x: x[[fill_col]].interpolate(method='linear'))
+
+print("Done.")
+
+
+# Calculate cases in relation to population for each NUTS ID
+
+print("\nCalculate cases in relation to population.")
+
+# Calculation
+covid_calc['cases_pop'] = covid_calc['cases'] / covid_calc['population'] * 10000
+
+print("Done.")
+
+
+# "Fork" weekly aggregates before further calculations
+print("\n'Fork' weekly aggregates before further calculations")
+
+# Group by nuts_id and aggregate by week
+covid_calc_weekly = (covid_calc
+                     .groupby(['nuts_id', pd.Grouper(key='date', freq='W-MON'), 'country', 'nuts_name'])[
+                         ['cases', 'cases_pop']]
+                     .sum()
+                     .reset_index()
+                     .sort_values('date')
+                     )
+
+# Rename columns in weekly data
+covid_calc_weekly = covid_calc_weekly.rename(columns={'cases': 'cases_w', 'cases_pop': 'cases_pop_w'})
+
+print("Done.")
+
+
+# Calculate 7 and 14 day rolling average for each NUTS ID
+
+print("\nCalculate 7/14 day rolling average for each NUTS ID.")
+
+covid_calc['moving7d_pop'] = (covid_calc.groupby('nuts_id')['cases_pop']
+                              .transform(lambda x: x.rolling(7, 1).mean().round(2)))
+
+covid_calc['moving14d_pop'] = (covid_calc.groupby('nuts_id')['cases_pop']
+                               .transform(lambda x: x.rolling(14, 1).mean().round(2)))
+
+print("Done.")
+
+
+# Calculate 4 week rolling average for aggregated weekly data
+
+print("\nCalculate 4 week rolling average for each NUTS ID (weekly data).")
+
+covid_calc_weekly['moving4w_pop'] = (covid_calc_weekly.groupby('nuts_id')['cases_pop_w']
+                                     .transform(lambda x: x.rolling(4, 1).mean().round(2)))
+
+print("Done.")
+
+
+# Calculate cumulated cases per population for each NUTS ID
+
+print("\nCalculate cumulated cases per population for each NUTS ID.")
+
+covid_calc['cumulated_pop'] = covid_calc.groupby('nuts_id')['cases_pop'].transform(pd.Series.cumsum)
+covid_calc_weekly['cumulated_pop_w'] = covid_calc_weekly.groupby('nuts_id')['cases_pop_w'].transform(pd.Series.cumsum)
+
+print("Done.")
+
+
+# Fill still missing values with a constant for 'no data available'
+
+print("\nFill still missing values with a constant for 'no data available'")
+
+# Daily data: Define columns to be filled
+no_data = ['cases', 'cases_pop', 'moving7d_pop', 'moving14d_pop', 'cumulated_pop']
+
+# Loop through columns and fill them
+for fill_col in no_data:
+    covid_calc[fill_col] = covid_calc[fill_col].fillna(value=-1)
+
+# Weekly data: Define columns to be filled
+no_data = ['cases_w', 'cases_pop_w', 'moving4w_pop', 'cumulated_pop_w']
+
+# Loop through columns and fill them
+for fill_col in no_data:
+    covid_calc_weekly[fill_col] = covid_calc_weekly[fill_col].fillna(value=-1)
+
+print("Done.")
+
+print("\nCalculations done.")
+
+##
+
+#
+# Export dataframe to CSV file
+#
+
+print("\nStart export.")
+
+# Define string to be added to file name if data is limited to certain time frame
+limit = ('_' + str(date_start) + '_' + str(date_end)) if limit_dates else ''
+
+# Define file name and export daily data to CSV
+file = 'data/covid-waves-data-clean' + limit + '.csv'
+covid_calc.to_csv(file)
+print("File saved as", file)
+
+# Define file name and export weekly data to CSV
+file = 'data/covid-waves-data-clean-weekly' + limit + '.csv'
+covid_calc_weekly.to_csv(file)
+print("File saved as", file)
+
+# Define file name and export weekly data to Excel file
+file = 'data/covid-waves-data-clean-weekly' + limit + '.xlsx'
+with pd.ExcelWriter(file) as writer:
+    covid_calc_weekly.to_excel(writer, sheet_name='Data')
+print("File saved as", file)
